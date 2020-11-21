f79f8bda4db76eaa1d5f65bfb18b44adebfbcc51
Chris <chrisliaw@antrapol.com>
2020-11-21 21:28:15 +0800

    Adding in test case

--- test/devops_helper_test.rb
+++ test/devops_helper_test.rb
@@ -1,11 +1,30 @@
 require "test_helper"
 
+require "devops_helper"
+
 class DevopsHelperTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::DevopsHelper::VERSION
+  include DevopsHelper::GemVersionHelper
+
+  def test_increase
+
+    test_cases = [
+      ["", 4, "0.0.0.1"],
+      ["", 1, "1.0"],
+      ["1.0",1,"2.0"],
+      ["1.0",2,"1.1.0"],
+      ["2.22",4,"2.22.0.1"],
+      ["2.14.2.11.22",1,"3.0.0.0.0"],
+      ["1",3,"1.0.1"],
+      ["1",1,"2.0"],
+      ["1.0", 9, "1.0.0.0.0.0.0.0.1"]
+    ]
+
+    test_cases.each do |tc|
+      res = increase_version(tc[0],tc[1])
+      puts "#{tc[0]} / #{tc[1]} / #{tc[2]} / #{res}"
+      assert(res == tc[2])
+    end
+
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
